deb40034bb7d27e06c157a05761d6178c98f58af
Chong Tze Yan <tze088@gmail.com>
2025-08-23 14:22:28 +0800

    Change chatbot name and greeting

---
+++ src/main/java/Bongo.java
@@ -0,0 +1,15 @@
+public class Bongo {
+    public static void main(String[] args) {
+        String hello =
+                " /\\__/\\    ／ ‾ ‾ ‾ ‾\n" +
+                "（　´∀｀） ＜　 " + "Hello! How can I help?\n" +
+                "（　　　） 　 ＼ ＿ ＿ ＿\n" +
+                " ｜ ｜　|\n" +
+                "（＿_)＿）";
+
+        String sep = "════════════════════════════════════════════════════════════════";
+        System.out.println(hello);
+        System.out.println(sep);
+        System.out.println("Bye Bye!");
+    }
+}
