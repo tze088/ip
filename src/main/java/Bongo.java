1302ca0135130e5de806d371e207a23f7aa7b80e
Chong Tze Yan <tze088@gmail.com>
2025-08-23 14:44:51 +0800

    Add simple echo functionality

--- src/main/java/Bongo.java
+++ src/main/java/Bongo.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Bongo {
     public static void main(String[] args) {
+        // Art and helper functions
         String hello = """
              /\\__/\\    ／ ‾ ‾ ‾ ‾
             （　´∀｀） ＜　 Hello! How can I help?
@@ -7,10 +10,29 @@ public class Bongo {
              ｜ ｜　|
             （＿_)＿）
             """;
-
         String sep = "════════════════════════════════════════════════════════════════";
+
+        java.util.function.Consumer<String> boxPrint = msg -> {
+            System.out.println(sep);
+            System.out.println("    " + msg);
+            System.out.println(sep);
+        };
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        // Program start
         System.out.println(hello);
-        System.out.println(sep);
-        System.out.println("Bye Bye!");
+
+        while (true) {
+            System.out.print("> "); // prompt
+            input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye Bye!");
+                break;
+            }
+            boxPrint.accept(input); // echo
+        }
     }
 }
