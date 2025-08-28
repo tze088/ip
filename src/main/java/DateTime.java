33b689c53e18a620470d8fd3c0772fdf7ce0afb5
Chong Tze Yan <tze088@gmail.com>
2025-08-29 02:16:13 +0800

    Add date and time parsing to Event class Refactor: Extract date/time handling into a dedicated class
    Encapsulating date and time handling ensures consistency and
    maintainability
    
    (cherry picked from commit 3a54b390973095141cf2164219464bf61dc240d7)

---
+++ src/main/java/DateTime.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DateTime {
+
+    private LocalDate date;
+
+    public DateTime(String date) throws Bongo.BongoException {
+        try {
+            this.date = LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        } catch (Exception e) {
+            throw new Bongo.BongoException("I only recognise the dd/MM/yyyy format.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd LLL yyyy");
+        return super.toString() + " (by: " + date.format(formatter) + ")";
+    }
+}
--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,22 +1,14 @@
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class Deadline extends Task {
 
-    private LocalDateTime by;
+    private DateTime by;
 
     public Deadline(String description, String by) throws Bongo.BongoException {
         super(description);
-        try {
-            this.by = LocalDateTime.parse(by, DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm"));
-        } catch (Exception e) {
-            throw new Bongo.BongoException("Invalid date format. Please use dd/MM/yyyy HHmm.");
-        }
+        this.by = new DateTime(by);
     }
 
     @Override
     public String toString() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd LLL yyyy, HH:mm");
-        return super.toString() + " (by: " + by.format(formatter) + ")";
+        return super.toString() + " (by: " + by + ")";
     }
 }
