9e27719d843dedf0eeb38229c3b1516058c02a9a
Chong Tze Yan <tze088@gmail.com>
2025-09-02 15:35:13 +0800

    Add find command for searching for tasks

--- src/main/java/bongo/Command.java
+++ src/main/java/bongo/Command.java
@@ -10,11 +10,12 @@ public class Command {
      * Enum representing the different command types.
      */
     public enum CommandType {
-        LIST, TODO, DEADLINE, EVENT, MARK, UNMARK, DELETE, BYE, UNKNOWN;
+        LIST, FIND, TODO, DEADLINE, EVENT, MARK, UNMARK, DELETE, BYE, UNKNOWN;
 
         private static CommandType from(String input) {
             return switch (input.toLowerCase()) {
                 case "list" -> LIST;
+                case "find" -> FIND;
                 case "todo" -> TODO;
                 case "deadline" -> DEADLINE;
                 case "event" -> EVENT;
