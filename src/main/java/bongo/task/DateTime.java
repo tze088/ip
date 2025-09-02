e2eec8c6997274965f2bb37fdc9ccb3939df9e5b
Chong Tze Yan <tze088@gmail.com>
2025-09-02 15:59:37 +0800

    Add Javadoc comments

--- src/main/java/bongo/task/DateTime.java
+++ src/main/java/bongo/task/DateTime.java
@@ -6,10 +6,21 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * The DateTime class represents a date in the dd/MM/yyyy format.
+ * It provides functionality to parse a string into a LocalDate and format it for display.
+ */
 public class DateTime implements Serializable {
 
     private LocalDate date;
 
+    /**
+     * Constructs a DateTime object by parsing the provided date string.
+     * The date string must be in the format "dd/MM/yyyy".
+     *
+     * @param date The date string to be parsed.
+     * @throws Bongo.BongoException If the date string does not match the required format.
+     */
     public DateTime(String date) throws Bongo.BongoException {
         try {
             this.date = LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
