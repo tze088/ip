6bdf3fb2663cb04567675b1ae845cb592f4d2be3
Chong Tze Yan <tze088@gmail.com>
2025-09-11 23:35:17 +0800

    Link GUI to command processing logic

--- src/main/java/bongo/gui/MainWindow.java
+++ src/main/java/bongo/gui/MainWindow.java
@@ -2,6 +2,8 @@ package bongo.gui;
 
 import bongo.Bongo;
 
+import javafx.animation.PauseTransition;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -9,6 +11,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
 /**
  * Controller for the main GUI.
  */
@@ -30,6 +34,9 @@ public class MainWindow extends AnchorPane {
     @FXML
     public void initialize() {
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+        dialogContainer.getChildren().add(
+                DialogBox.getBongoDialog("Oh, it's you. What is it now?", bongoImage)
+        );
     }
 
     /** Injects the Bongo instance */
@@ -50,5 +57,11 @@ public class MainWindow extends AnchorPane {
                 DialogBox.getBongoDialog(response, bongoImage)
         );
         userInput.clear();
+
+        if (response.equals("Bye Bye!")) {
+            PauseTransition pause = new PauseTransition(Duration.millis(500));
+            pause.setOnFinished(event -> Platform.exit());
+            pause.play();
+        }
     }
 }
